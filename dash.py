0b23dfa562541b005abbbb3cb4cb82186dab8fcb
alemelis <a@alemel.is>
2025-12-05 21:42:09 +0000

    dwnld playlist

--- dash.py
+++ dash.py
@@ -8,15 +8,15 @@ SAVEDIR = Path('/mnt/owntone/music/')
 url = st.text_input(label="URL")
 title = st.text_input(label="Title")
 author = st.text_input(label="Author")
+album = st.text_input(label="Album")
 delay = st.number_input(label="Delay (s)", value=0)
+
+playlist = st.checkbox(label="playlist", value=False)
 
 if st.button("Run Command"):
     if not url:
         st.error("need the URL!")
         st.stop()
-
-    if "&" in url:
-        url = url.split("&")[0]
 
     st.info("Downloading...")
     placeholder = st.empty()  # this area will be updated live
@@ -24,11 +24,22 @@ if st.button("Run Command"):
 
     # Run the command and capture output as it happens
     savedir = SAVEDIR / author
+    dirname = author
+    playlist_flag = "--no-playlist"
+    if playlist:
+        dirname = f"{author}-{album}"
+        savedir = SAVEDIR / dirname
+        playlist_flag = "--yes-playlist"
     savedir.mkdir(parents=True, exist_ok=True)
     if title == "":
-        title = '"%(title)s"'
+        if playlist:
+            title = '"%(playlist_index)02d-%(title)s"'
+        else:
+            title = '"%(title)s"'
+    cmd = ["yt-dlp", playlist_flag, "-x", "--audio-format", "m4a", "-P", str(savedir), "-o", title, url]
+    st.text(" ".join(cmd))
     process = subprocess.Popen(
-        ["yt-dlp", "-x", "--audio-format", "m4a", "-P", str(savedir), "-o", title, url],
+        cmd,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
         text=True,
@@ -49,7 +60,7 @@ if st.button("Run Command"):
     if process.returncode == 0:
         placeholder.text("")
         st.success("✅")
-        os.system(f"ls {savedir}/*.m4a > {savedir}/{author}.m3u")
+        os.system(f"ls {savedir}/*.m4a > {savedir}/{dirname}.m3u")
     else:
         st.error(f"Failed with code: {process.returncode}")
         st.stop()
@@ -75,7 +86,7 @@ if st.button("Run Command"):
             placeholder.text("")
             st.success("✅ Command completed successfully!")
             os.system(f"mv /tmp/temp_audio.m4a {dst}")
-            os.system(f"ls {savedir}/*.m4a > {savedir}/{author}.m3u")
+            os.system(f"ls {savedir}/*.m4a > {savedir}/{dirname}.m3u")
         else:
             st.error(f"❌ Command failed with code {process.returncode}")
             st.stop()
